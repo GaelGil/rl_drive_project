ba7a04821c13997926ef84c93365c599624f3a3a
Gael Gil <gaelgilbello27@gmail.com>
2026-06-08 13:27:36 -0700

    updating the project structure

--- src/game/action.rs
+++ src/game/action.rs
@@ -5,3 +5,18 @@ pub enum Action {
     MoveRight,
     Shoot,
 }
+
+impl Action {
+    pub fn from_index(index: usize) -> Self {
+        match index {
+            1 => Self::MoveLeft,
+            2 => Self::MoveRight,
+            3 => Self::Shoot,
+            _ => Self::NoOp,
+        }
+    }
+
+    pub fn action_count() -> usize {
+        return 4;
+    }
+}
