edae239d23b7dd7904a1e9fb70ce96dd6e9db5cd
Gael Gil <gaelgilbello27@gmail.com>
2026-06-08 00:21:33 -0700

    added dependency

--- src/game/update.rs
+++ src/game/update.rs
@@ -109,7 +109,7 @@ fn check_game_over(state: &mut GameState) {
         if (asteroid.is_alive
             && state.player.is_alive
             && overlaps(&asteroid.body, &state.player.body))
-            || (asteroid.is_alive && asteroid.body.bottom() >= state.base_y)
+            || asteroid.body.bottom() >= state.base_y
         {
             state.player.is_alive = false;
             state.game_over = true;
