b57ff8cbfd2a82ee7fce442ee2f7b4698805d683
gaelgil <gaelgilbello27@gmail.com>
2026-07-18 19:29:15 -0700

    more comments

--- src/game/render.rs
+++ src/game/render.rs
@@ -2,8 +2,9 @@ use crate::game::state::GameState;
 use macroquad::prelude::*;
 
 pub fn draw_game(state: &GameState) {
+    // pass in state as reference only since we are just reading the value
     clear_background(BLACK);
-    // access actual value
+    // draw the player if alive
     if state.player.is_alive {
         draw_rectangle(
             state.player.body.x,
@@ -14,8 +15,8 @@ pub fn draw_game(state: &GameState) {
         )
     }
 
-    // iterate by reference so we can read aliens with out
-    // chainging ownership
+    // iterate by reference so we can read asteroids
+    // we are only reading them not modifying them
     for asteroid in &state.asteroids {
         if asteroid.is_alive {
             draw_rectangle(
@@ -40,6 +41,7 @@ pub fn draw_game(state: &GameState) {
         }
     }
 
+    // display the score and current round
     draw_text(&format!("Score: {}", state.score), 20.0, 30.0, 30.0, WHITE);
     draw_text(
         &format!("Round: {}/{}", state.current_round, state.rounds),
@@ -49,6 +51,18 @@ pub fn draw_game(state: &GameState) {
         WHITE,
     );
 
+    // if game is over
+    if state.current_round == state.rounds {
+        draw_text(
+            "You Won!",
+            state.screen_width * 0.5 - 100.0,
+            state.screen_height * 0.5,
+            40.0,
+            WHITE,
+        );
+    }
+
+    // if game is over
     if state.game_over {
         draw_text(
             "Game Over",
